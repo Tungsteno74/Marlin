969c89afb79da1572c3c652c4f7846df97dc94fa
Tungsteno74 <tungsteno74@yahoo.it>
2018-03-26 05:57:09 +0200

    Added gt2560 Rev. A EFB pins file

---
+++ Marlin/pins_GT2560_REV_A_EFB.h
@@ -0,0 +1,45 @@
+/**
+ * Marlin 3D Printer Firmware
+ * Copyright (C) 2016 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ *
+ * Based on Sprinter and grbl.
+ * Copyright (C) 2011 Camiel Gubbels / Erik van der Zalm
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/**
+ * Geeetech GT2560 Revision A board pin assignments, based on the work of
+ * George Robles (https://georges3dprinters.com) and
+ * Richard Smith <galorin@gmail.com>
+ */
+
+#if !defined(__AVR_ATmega1280__) && !defined(__AVR_ATmega2560__)
+  #error "Oops!  Make sure you have 'Arduino Mega' selected from the 'Tools -> Boards' menu."
+#endif
+
+#ifndef BOARD_NAME
+  #define BOARD_NAME "GT2560 Rev.A (efb)"
+#endif
+
+#include "pins_GT2560_REV_A.h"
+
+
+//
+// Heaters / Fans
+//
+#undef HEATER_1_PIN
+#define FAN1_PIN            3 //use fan_1 on pin 3 instead of heater_1
+
