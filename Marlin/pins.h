9446cb77390e121afac2fb78fb65ab3aceccb59b
Tungsteno74 <tungsteno74@yahoo.it>
2018-03-26 05:59:39 +0200

    Added pins inclusion for EFB

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -170,6 +170,8 @@
   #include "pins_GT2560_REV_A.h"      // ATmega1280, ATmega2560
 #elif MB(GT2560_REV_A_PLUS)
   #include "pins_GT2560_REV_A_PLUS.h" // ATmega1280, ATmega2560
+#elif MB(GT2560_REV_A_EFB)
+  #include "pins_GT2560_REV_A_EFB.h"  // ATmega1280, ATmega2560
 #elif MB(SILVER_GATE)
   #include "pins_SILVER_GATE.h"       // ATmega2561
 
