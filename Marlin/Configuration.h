1e195c6eb19076589c0f8842fc14ec665ea98ed1
Tungsteno74 <tungsteno74@yahoo.it>
2018-04-13 17:51:36 +0200

    x plane dimension changed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -791,7 +791,7 @@
 
 // The size of the print bed
 #define X_BED_SIZE 190
-#define Y_BED_SIZE 208
+#define Y_BED_SIZE 206
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
